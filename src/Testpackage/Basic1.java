6871091245a5ac12ff752f7918f491854352da0c
shahadat hossain <ahmedmilon19902yahoo.com>
2019-08-07 12:28:01 -0400

    first comment

---
+++ src/Testpackage/Basic1.java
@@ -0,0 +1,18 @@
+package Testpackage;
+
+import org.testng.annotations.Test;
+
+
+public class Basic1 {
+	
+	
+	@Test
+	public void hi() {
+	System.out.println("sdas");
+	}
+	
+	@Test
+	public void bye() {
+		System.out.println("bye");
+	}
+}
